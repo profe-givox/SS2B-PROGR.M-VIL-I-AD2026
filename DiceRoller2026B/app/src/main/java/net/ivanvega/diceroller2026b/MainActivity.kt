8e200cecc889c5a15133bc210eeca086a3a593d2
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-09-03 08:56:18 -0600

    APrendimos a: Variables de estado para recomposicion Remember para recordar el estado Manejo de multiples idiomas

--- DiceRoller2026B/app/src/main/java/net/ivanvega/diceroller2026b/MainActivity.kt
+++ DiceRoller2026B/app/src/main/java/net/ivanvega/diceroller2026b/MainActivity.kt
@@ -7,18 +7,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.ivanvega.diceroller2026b.ui.theme.DiceRoller2026BTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,17 +47,38 @@ class MainActivity : ComponentActivity() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
-    var result = 1
+    var result  by remember { mutableStateOf(0) }
+    var cajatexto  by remember { mutableStateOf("") }
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.dice_1),
-            contentDescription = "1",
-        )
+        val imageResource = when(result){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
+        var eventoCaja : (String) -> Unit
+                = { texto: String ->
+                    cajatexto  = texto
+                    Log.d("XXXMainActivity", "Caja de texto $cajatexto")
+                }
+
+        eventoCaja = ::println
+
+        TextField(value = cajatexto,
+            onValueChange = { cajatexto = it } )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = if(result == 0) stringResource(R.string.laimagen) else result.toString(),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             result = (1..6).random()
             Log.d("XXXMainActivity", "Button Clicked $result")
